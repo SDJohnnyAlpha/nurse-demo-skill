a05fb108b45608b654db71e7e70377a1745d6826
Stephen Barnes <stephen.barnes.wex@gmail.com>
2019-02-15 14:18:00 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,13 +6,17 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
+Logger = getLogger(__name__)
 
-class NurseDemo(MycroftSkill):
+class NurseDemoSkill(MycroftSkill):
     def __init__(self):
-        super(NurseDemo, self).__init__(name="NurseDemo")
+        super(NurseDemoSkill, self).__init__(name="NurseDemo")
 
         def initialize(self):
-            where_am_i_intent = IntentBuilder("WhereAmIintent").require("where.am.i").build()self.register_intent(where_am_i_intent, self.handle_where_am_i_intent)
+
+            where_am_i_intent = IntentBuilder("WhereAmIintent").
+                require("where.am.i").build()
+                self.register_intent(where_am_i_intent, self.handle_where_am_i_intent)
             the_nurse_is_intent = IntentBuilder("TheNurseIsintent").
                 require("the.nurse.is").build()self.register_intent(the_nurse_is_intent, self.handle_the_nurse_is_intent)
             the_doctor_is_intent = IntentBuilder("TheDoctorIsintent").
@@ -29,25 +33,25 @@ class NurseDemo(MycroftSkill):
     # It is now necessary to define methods that handle each of the Intents
 
     def handle_where_am_i_intent(self, message):
-        self.speak_dialog("whereami")
+#        self.speak_dialog("whereami")
 
-    def handle_the_nurse_is_intent(self, message):
-        self.speak_dialog("whoisthenurse")
+#    def handle_the_nurse_is_intent(self, message):
+#        self.speak_dialog("whoisthenurse")
 
-    def handle_the_doctor_is_intent(self, message):
-        self.speak_dialog("whoisthedoctor")
+#    def handle_the_doctor_is_intent(self, message):
+#        self.speak_dialog("whoisthedoctor")
 
-    def handle_go_home_intent(self, message):
-            self.speak("releaserequest")
+#    def handle_go_home_intent(self, message):
+#            self.speak("releaserequest")
 
-    def handle_diet_intent(self, message):
-        self.speak_dialog("dietupdate")
+#    def handle_diet_intent(self, message):
+#        self.speak_dialog("dietupdate")
+#
+#    def handle_medication_intent(self, message):
+#        self.speak_dialog("medicationupdate")
 
-    def handle_medication_intent(self, message):
-        self.speak_dialog("medicationupdate")
-
-    def handle_doctor_visit_intent(self, message):
-        self.speak_dialog("doctorroundsupdate")
+#    def handle_doctor_visit_intent(self, message):
+#        self.speak_dialog("doctorroundsupdate")
 #    @intent_file_handler('demo.nurse.intent')
 #    def handle_demo_nurse(self, message):
 #        self.speak_dialog('demo.nurse')
